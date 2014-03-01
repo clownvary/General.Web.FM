93b0f510ae36bf4ddd992c1f5c0b533611a0b868
clownvary <clownvary@gmail.com>
2014-03-01 12:24:50 +0800

    d

--- General.Web.UI/General.Ioc/Aurofac.cs
+++ General.Web.UI/General.Ioc/Aurofac.cs
@@ -11,18 +11,18 @@ using Autofac.Integration.Mvc;
 using General.Service.Contract;
 using General.Service;
 using General.Model;
+using System.Configuration;
+using System.IO;
 namespace General.Ioc
 {
     public class Aurofac
     {
         public static ContainerBuilder RegisterService()
         {
+            string path = System.Configuration.ConfigurationManager.AppSettings["path"];
             var builder = new ContainerBuilder();
-
             var assembles = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.StartsWith("General")).ToList();
-
-            assembles.Add(Assembly.LoadFrom(@"D:\Documents\GitHub\General.Web.FM\General.Web.UI\General.Service\bin\Debug\General.Service.dll"));
-           
+            assembles.Add(Assembly.LoadFrom(path+@"\General.Service.dll"));
             builder.RegisterControllers(assembles.ToArray());
           // builder.RegisterType<TeacherService>().As<ITeacherService>();
             builder.RegisterAssemblyTypes(assembles.ToArray()).Where(a => a.Name.EndsWith("Repo")).AsImplementedInterfaces().InstancePerLifetimeScope();
